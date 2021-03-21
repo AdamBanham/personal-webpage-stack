b6313e9b466e1e7257ef3bdce11698668a3fb378
Adam Peter Banham <n7176546@qut.edu.au>
2021-03-21 18:57:26 +1000

    hmm public path doesnt remove issue on cloudfront.

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,2 @@
-const { useCssModule } = require("vue");
-
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production"
-        ? '/' // cloudfront appends s3 origin so we don't need to change publicPath
-        : '/'
 }
