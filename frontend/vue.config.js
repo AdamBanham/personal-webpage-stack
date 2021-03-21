952cace932ae374aa694b8b07c8f9402962f4b46
Adam Peter Banham <n7176546@qut.edu.au>
2021-03-21 17:58:46 +1000

    remove public path requirement now that cloud front is hosting.

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,6 +2,6 @@ const { useCssModule } = require("vue");
 
 module.exports = {
     publicPath: process.env.NODE_ENV === "production"
-        ? '/adambanham.io/'
+        ? '/' // cloudfront appends s3 origin so we don't need to change publicPath
         : '/'
 }
