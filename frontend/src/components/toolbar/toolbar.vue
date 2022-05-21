bcff0eaad6e616580784696f6c9d5090f2fb8344
Adam Peter Banham <n7176546@qut.edu.au>
2022-05-21 11:38:16 +1000

    updated style of toolbar

--- frontend/src/components/toolbar/toolbar.vue
+++ frontend/src/components/toolbar/toolbar.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="toolbar">
+    <div class="name">
+        Adam P. Banham
+    </div>
+    <div  class="choices">
     <div
       class="choice"
       @click="moveTo('home')"
@@ -30,6 +34,7 @@
     >
       Contact
     </div>
+    </div>
   </div>
 </template>
 
@@ -48,29 +53,43 @@ methods : {
 @import "@/styles/colours_ant.sass"
 .toolbar
     width: 100% 
-    height: 50px
-    background-color: $green-3
-    box-shadow: $green-1 0px 5px 5px 0.5px
-    border-radius: 0px 0px 15px 15px
+    height: 40px
+    background: linear-gradient($cyan-1, $cyan-5 90%)
+    box-shadow: $green-1 0px 5px 3px 0.25px
+    border-radius: 0px 0px 5px 5px
     display: inline-flex
-    justify-content: center
-    align-items: center
-    .choice 
+    justify-content: flex-start
+    .choices
+      display: inline-flex
+      justify-content: center
+      align-items: center
+      width: 100%
+      .choice 
         display: inline-flex
         justify-content: center
         align-items: center
-        padding-left: 10px
-        padding-right: 10px
-        margin-left: 15px
-        margin-right: 15px
-        height: 50px
+        padding-left: 15px
+        padding-right: 15px
+        margin-left: 3px
+        margin-right: 3px
+        height: 40px
         text-align: center 
         vertical-align: middle
         border-radius: 7.5px
-        color: $green-10 
+        color: $blue-1
         cursor: pointer
         font-size: 22px
         &:hover 
-            background-color: $green-9
-            color: $green-1
+            background-color: $cyan-5
+            color: $cyan-1
+    .name
+      margin-left: 25px
+      font-size: 22px
+      color: $blue-1
+      width: 200px
+      display: inline-flex
+      justify-content: center
+      align-items: center
+      opacity: 50%
+    
 </style>
