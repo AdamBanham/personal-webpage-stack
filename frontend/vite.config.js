156d91f146ceb7a35075a1d0d6c49d2b9093eae8
Adam Peter Banham <n7176546@qut.edu.au>
2022-10-04 20:27:06 +1000

    touch ups to vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,20 +4,21 @@ import { defineConfig } from 'vite'
 const path = require("path");
 import vue from '@vitejs/plugin-vue'
 
+var paths = path.resolve(__dirname, "./src")
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
     watch: {
-      ignored: ['!**/node_modules/your-package-name/**'],
+      ignored: ['**/node_modules/**'],
       usePolling: true
     },
     
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@" : paths,
     },
-  },
+  }
 })
